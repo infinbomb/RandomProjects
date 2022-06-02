d04cafc3b231717ef981bb0d7d349e687dfebbeb
infinbomb <99835573+infinbomb@users.noreply.github.com>
2022-06-02 14:34:55 -0400

    a

--- alarm.py
+++ alarm.py
@@ -1,4 +1,30 @@
-from tkinter import *
-import datetime
+from tkinter import * # GUI
 import time
-import winsound
+import winsound # for sound
+
+app = Tk()
+app.title("myalarmclock")
+app.geometry("600x400")
+
+def clock():
+    hour = time.strftime("%I")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
+    day = time.strftime("%A")
+    am_pm = time.strftime("%p")
+    time_zone = time.strftime("%Z")
+
+    my_label.config(text=hour + ":" + minute + ":" + second + " " + am_pm)
+    my_label.after(1000, clock)
+    
+    my_label2.config(text=time_zone + " " + day)
+    
+my_label = Label(app, text="", font=("Helvetica", 48), fg="green", bg="black")
+my_label.pack(pady=20)
+
+my_label2 = Label(app, text="", font=("Helvetica", 14))
+my_label2.pack(pady=100)
+
+clock()
+
+app.mainloop()
--- datetimepratice.py
+++ datetimepratice.py
@@ -1,5 +1,11 @@
-from datetime import date
-import time
+import tkinter as tk
 
-today = date.today()
-print(today)
+window = tk.Tk()
+
+greeting = tk.Label(text="Hello, Tkinter!")
+greeting.pack()
+
+window.mainloop() 
+# If you don’t include window.mainloop() at the end of a 
+# program in a Python file, then the Tkinter application 
+# will never run, and nothing will be displayed.
